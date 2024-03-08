7ad7db866e0ef191e052cc17f42fdf05f7ccc920
SCU Imaginarium (Aargorn) <mvfollosco@gmail.com>
2024-03-08 11:58:27 -0800

    The Door
    It works!

--- Assets/Scripts/MazeEnter.cs
+++ Assets/Scripts/MazeEnter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class MazeEnter : MonoBehaviour
 {
@@ -8,20 +9,41 @@ public class MazeEnter : MonoBehaviour
     [SerializeField] GameObject door;
     string door1 = "door1";
     AudioSource audioData;
+    [SerializeField] GameObject keyhole;
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject floor;
+    XRSocketInteractor socket;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        socket = GetComponent<XRSocketInteractor>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        socketCheck();
     }
 
-    private void OnTriggerEnter1(Collider other)
+    public void socketCheck()
+    {
+
+        if (socket.hasSelection)
+        {
+            //move player
+            Player.transform.position = new Vector3(3.5f, 0, -1);
+            //Player.transform.Rotate = new Vector3(3.5f, 0, -1);
+            //Try this transform.rotation *= Quaternion.Euler(0,90,0);
+            //delete key and door
+            Destroy(door);
+            Destroy(key);
+            Destroy(floor);
+        }
+    }
+
+    /*
+    private void OnTriggerInSocket(Collider other)
     {
         Debug.Log("entered");
         if (other.gameObject.tag == door1)
@@ -29,11 +51,11 @@ public class MazeEnter : MonoBehaviour
             //move player
             transform.position = new Vector3(3.5f, 0, -1);
             //delete door
-            Destroy(gameObject);
+            Destroy(door);
         }
         
 
-        /*
+        
         if (other.gameObject.tag == purpleTag)
         {
             audioData = other.GetComponent<AudioSource>();
@@ -43,6 +65,6 @@ public class MazeEnter : MonoBehaviour
         {
             Destroy(other.gameObject);
         }
-        */
     }
+        */
 }
